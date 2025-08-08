d8369e944a0764bedbd6bf171a1da26d29255112
stolleym <stolleym@gmail.com>
2025-08-08 22:35:48 +1000

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,15 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./app/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
-    "./pages/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
 export default config;
